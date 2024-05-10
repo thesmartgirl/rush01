a5fda93742928a1e51a797cc5e57ed506cf3a45b
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-05-10 13:20:26 +0300

    Update common.c

--- common.c
+++ common.c
@@ -1,19 +1,16 @@
 #include <unistd.h>
 
-// Prints a single char
 void ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-//prints a list of characters
 void ft_putstr(char *str)
 {
 	while (*str)
 		write(1, str++, 1);
 }
 
-//gives the length of a list of character (strlen)
 int ft_strlen(char *str)
 {
 	int i;
@@ -24,26 +21,23 @@ int ft_strlen(char *str)
 	return (i);
 }
 
-//similar to (atoi() in stdlib.h)
 int ft_atoi(char *str)
 {
 	int i;
 	int nbr;
-	int nbrminus;
+	int sign;
 
 	i = 0;
 	nbr = 0;
-	nbrminus = 0;
+	sign = 1;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
 	while (str[i] == '-' || str[i] == '+')
 		if (str[i++] == '-')
-			nbrminus++;
+			sign *= -1;
 	while (str[i] >= '0' && str[i] <= '9')
 		nbr = nbr * 10 + (str[i++] - '0');
-	if (nbrminus % 2 == 1)
-		return (nbr * -1);
-	return (nbr);
+	return (nbr * sign);
 }
 
 //prints a number in the standart output
